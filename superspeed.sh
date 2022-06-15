9a58bfb6d6edd9ae9407bab139d120b984515f84
neil <github@neilpang.com>
2022-06-15 22:28:21 +0800

    Update superspeed.sh

--- superspeed.sh
+++ superspeed.sh
@@ -50,7 +50,7 @@ speed_test(){
 
 preinfo() {
 	echo "———————————————————SuperSpeed 全面测速版——————————————————"
-	echo "       curl -L https://neilpang.com/speed | bash"
+	echo "       bash <(curl -Lso- https://git.io/superspeed)"
 	echo "       全部节点列表:  https://git.io/superspeedList"
 	echo "       节点更新: 2020/04/09  | 脚本更新: 2022/06/15"
 	echo "——————————————————————————————————————————————————————————"
